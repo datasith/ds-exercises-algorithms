413be6471026e3ddf6de07db77446a4d6f45fafc
datsith <zabala@caltech.edu>
2022-04-30 19:33:52 -0700

    one more

--- arraymax.py
+++ arraymax.py
@@ -1,19 +1,21 @@
 import unittest
 
 def selection_sort(array):
-    idx = 0
+    idx = 0 
     while idx < len(array):
-        print('selection sort: ' + str(array))
+        print('sorting: ', array)
         for i in range(idx, len(array)):
             if array[i] < array[idx]:
-                array[idx], array[i] = array[i], array[idx]
+                array[i], array[idx] = array[idx], array[i]
         idx += 1
     return array
 
 class CustomTest(unittest.TestCase):
     def test01(self):
         input = [4, 7, 2, 8, 10, 9]
-        self.assertEqual( selection_sort(input)[-1], max(input) )
+        output = selection_sort(input)
+        print(output)
+        self.assertEqual( output[-1], max(input) )
 
 if __name__ == '__main__':
     unittest.main()
