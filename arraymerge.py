4d73e5ac6b47244c37b92fd4834788b559de3f97
datsith <zabala@caltech.edu>
2022-05-18 19:10:44 -0700

    adding unittests

--- arraymerge.py
+++ arraymerge.py
@@ -1,29 +1,34 @@
-def merge(left, right):
-    i = 0
-    j = 0
-    merged = []
-    
-    left = list(filter(None, left))
-    left = sorted(left)
-    right = sorted(right)
+import unittest
 
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            merged.append(left[i])
-            i += 1
+def merge(arr1, arr2):
+    i = j = 0
+    n, m = len(arr1), len(arr2)
+    output = []
+
+    arr1 = sorted(arr1)
+    arr2 = sorted(arr2)
+
+    while i < n and j < m:
+        if arr1[i] <= arr2[j]:
+            output.append(arr1[i])
+            i+=1
         else:
-            merged.append(right[j])
-            j += 1
-    
-    if i < len(left):
-        merged += left[i:]
-    if j < len(right):
-        merged += right[j:]
-    return merged
+            output.append(arr2[j])
+            j+=1            
+
+    if i < n:
+        output += arr1[i:]
+    elif j < m:
+        output += arr2[j:]
+
+    return output
+
+class CustomTest(unittest.TestCase):
+    arr1 = [0,1,3,9]
+    arr2 = [2,4,6,8,10]    
+    def test01(self):
+        ans = merge(self.arr1, self.arr2)
+        self.assertEqual(ans, [0, 1, 2, 3, 4, 6, 8, 9, 10])
 
 if __name__ == '__main__':
-    x = [1,2,3,None,None,None]
-    y = [4,2,3]
-    ans = merge(x,y)
-    assert ans == [1,2,2,3,3,4]
-    print(ans)
+    unittest.main()
